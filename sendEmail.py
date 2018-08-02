3560212e21cfbcb223cc075f59424b4b0b2d38b2
yzh <you_zhihong@aliyun.com>
2018-08-02 14:08:12 +0800

    1. 每次发送10个邮箱 2. 每次发送之间随机间隔1~10分钟 3. 第次新的发送需要重新连接SMTP服务器

--- sendEmail.py
+++ sendEmail.py
@@ -6,16 +6,18 @@
 # @File     : sendEmail.py
 # @Software : PyCharm
 
-import smtplib
+import smtplib, random
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from time import sleep
 
-fromaddr = 'admin@uniex.one'
-toaddrs = '379975166@qq.com'
+fromaddr = 'support@uniex.one'
+with open('/home/ec2-user/mail_lists/mail_163_20180730') as f:
+    mail_lists = f.read().splitlines()
+toaddrs = []
 msg = MIMEMultipart('alternative')
 msg['Subject'] = '邮件订阅回执'
 msg['From'] = fromaddr
-msg['To'] = toaddrs
 
 with open('/home/ec2-user/mail_msg/content.txt') as f1:
     msg_txt = f1.read()
@@ -28,8 +30,21 @@ msg_part2 = MIMEText(msg_html, 'html')
 msg.attach(msg_part1)
 msg.attach(msg_part2)
 
-server = smtplib.SMTP('smtp.dot618.com',587)
-server.starttls()
-server.login('yzh@dot618.com', 'slee@1092')
-server.sendmail(fromaddr, toaddrs, msg.as_string())
-server.quit()
+for addr in mail_lists:
+    toaddrs.append(addr)
+    if len(toaddrs) < 10:
+        continue
+    if  len(toaddrs) == 10:
+        msg['To'] = ",".join(toaddrs)
+        with smtplib.SMTP('smtp.dot618.com',587) as server:
+            server.starttls()
+            server.login('yzh@dot618.com', 'slee@1092')
+            server.sendmail(fromaddr, toaddrs, msg.as_string())
+        toaddrs.clear()
+    sleep(random.randrange(60, 600))
+if toaddrs:
+    msg['To'] = ",".join(toaddrs)
+    with smtplib.SMTP('smtp.dot618.com', 587) as server:
+        server.starttls()
+        server.login('yzh@dot618.com', 'slee@1092')
+        server.sendmail(fromaddr, toaddrs, msg.as_string())
